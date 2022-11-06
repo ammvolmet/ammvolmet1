acf6e2181d08e6edaba9b342c5279e6df24e7782
ammvolmet <117588100+ammvolmet@users.noreply.github.com>
2022-11-06 22:08:50 +0530

    Add files via upload

---
+++ A1P11.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int h, m;
+    printf("enter hour:\n");
+    scanf("%d", &h);
+    printf("enter minute:\n");
+    scanf("%d", &m);
+    printf("%d Hour and %d Minute", h, m);
+    return 0;
+}
